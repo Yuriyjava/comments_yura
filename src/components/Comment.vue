890da3091168d544590fca3a1a9440aec051c27b
Yurii Pridatko <yp0620@gmail.com>
2018-08-16 21:35:46 +0300

    component-comment

---
+++ src/components/Comment.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+     <div>{{comment.author}}</div>
+     <div>{{comment.text}}</div>
+     <div>{{comment.created_at}}</div>
+     <div v-if="comment.created_at !== comment.updated_at">{{comment.updated_at}}</div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Comment',
+  props: {
+    comment:{}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card{
+  width: 400px;
+  min-height: 150px;
+  border: 1px solid  lightgreen;
+  border-radius: 5px;
+  margin-bottom: 15px;
+  background-color: aquamarine;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
+<div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  </div>
+
+    <router-link to="/about">About</router-link>
+    <Comment v-for="comment in validComments" :comment="comment" :key="comment.id"></Comment>
+
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Comment from '@/components/Comment.vue'
+import API from '@/services/api.service'
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'home',
+    components: {
+        Comment
+    },
+    created() {
+        this.getComments()
+    },
+    data() {
+        // debugger;
+        return {
+            comments: []
+        };
+    },
+    computed: {
+        validComments: function () {
+            return this.comments.filter(function (comment) {
+               // eslint-disable-next-line
+              debugger
+                return comment.id
+            });
+        }
+    },
+    methods: {
+        getComments() {
+          
+            API.get("comments").then((e) => {
+               
+                if (e.data.success) {
+                    this.comments = e.data;
+                }
+            });
+        }
+    }
 }
 </script>
+
+<style scoped>
+.home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
